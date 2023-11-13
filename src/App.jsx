e1e5b7691864c6e90a0eaacb1d3b283f6bf1b991
devesh1808 <devesh1808@gmail.com>
2023-11-13 21:02:44 +0530

    added UserContextProvider tag in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import UserContextProvider from './context/UserContextProvider';
 import './App.css'
 
 function App() {
 
   return (
-    <>
+    <UserContextProvider>
       <h1>Hello</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
